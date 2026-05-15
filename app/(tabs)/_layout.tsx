49fe8e25487ebcfb7177533f1941cf70cae818b4
mcastre1 <miguelcastrejongal@gmail.com>
2026-05-15 13:43:36 -0700

    Icons on tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,33 @@
+import { AntDesign } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { Text } from 'react-native';
 
 export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="calendar"
-        options={{ title: "Calendar" }}
+        options={({ route }) => ({
+          title: "Calendar",
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="calendar" size={24} color={focused ? 'black' : 'gray'} />
+          ),
+          tabBarLabel: ({ focused, color }) => (
+            <Text style={{ color: focused ? 'black' : 'gray' }}>Calendar</Text>
+          ),
+        })}
       />
       <Tabs.Screen
         name="history"
-        options={{ title: "History" }}
+        options={({ route }) => ({
+          title: "History",
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="bar-chart" size={24} color={focused ? 'black' : 'gray'} />
+          ),
+          tabBarLabel: ({ focused, color }) => (
+            <Text style={{ color: focused ? 'black' : 'gray' }}>History</Text>
+          ),
+        })}
       />
     </Tabs>
   );;
