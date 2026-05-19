172c657657bb1408a7d76ae989a435cf12cd7671
mcastre1 <miguelcastrejongal@gmail.com>
2026-05-19 12:29:24 -0700

    Moved constant variables to their own global file.

---
+++ src/constants/exercises.ts
@@ -0,0 +1,95 @@
+export const bodySection = [
+    { name: "Upper Body" },
+    { name: "Lower Body" },
+] as const;
+
+export const lowerBodyExercises = {
+    quads: [
+    "Barbell Back Squat",
+    "Barbell Front Squat",
+    "Dumbbell Goblet Squat",
+    "Dumbbell Split Squat",
+    "Bulgarian Split Squat",
+    "Barbell Hack Squat",
+    "Squat Machine (Standard)",
+    "Squat Machine (Narrow Stance)",
+    "Squat Machine (Wide Stance)"
+  ],
+
+  hamstrings: [
+    "Barbell Romanian Deadlift",
+    "Barbell Stiff-Leg Deadlift",
+    "Dumbbell Romanian Deadlift",
+    "Dumbbell Stiff-Leg Deadlift"
+  ],
+
+  glutes: [
+    "Barbell Hip Thrust",
+    "Dumbbell Hip Thrust",
+    "Barbell Glute Bridge",
+    "Dumbbell Glute Bridge",
+    "Dumbbell Step-Ups",
+    "Dumbbell Lunges (Forward)",
+    "Dumbbell Lunges (Reverse)",
+    "Dumbbell Lunges (Walking)"
+  ],
+
+  calves: [
+    "Dumbbell Standing Calf Raise",
+    "Barbell Standing Calf Raise",
+    "Dumbbell Seated Calf Raise"
+  ]
+} as const;
+
+export const upperBodyExercises = {
+  chest: [
+    "Flat Barbell Bench Press",
+    "Incline Barbell Bench Press",
+    "Decline Barbell Bench Press",
+    "Flat Dumbbell Bench Press",
+    "Incline Dumbbell Bench Press",
+    "Decline Dumbbell Bench Press",
+    "Dumbbell Flyes (Flat)",
+    "Dumbbell Flyes (Incline)",
+    "Dumbbell Pullover"
+  ],
+
+  back: [
+    "Barbell Bent-Over Row",
+    "Pendlay Row",
+    "Single-Arm Dumbbell Row",
+    "Chest-Supported Dumbbell Row",
+    "Barbell Shrugs",
+    "Dumbbell Shrugs",
+    "Barbell Deadlift",
+    "Romanian Deadlift"
+  ],
+
+  shoulders: [
+    "Barbell Overhead Press",
+    "Dumbbell Shoulder Press",
+    "Arnold Press",
+    "Dumbbell Lateral Raise",
+    "Dumbbell Front Raise",
+    "Dumbbell Rear Delt Fly",
+    "Barbell Upright Row",
+    "Dumbbell Upright Row"
+  ],
+
+  biceps: [
+    "Barbell Curl",
+    "EZ-Bar Curl",
+    "Dumbbell Curl",
+    "Hammer Curl",
+    "Incline Dumbbell Curl",
+    "Concentration Curl"
+  ],
+
+  triceps: [
+    "Barbell Skull Crushers",
+    "Dumbbell Skull Crushers",
+    "Dumbbell Overhead Triceps Extension",
+    "Close-Grip Barbell Bench Press",
+    "Dumbbell Kickback"
+  ]
+} as const;
