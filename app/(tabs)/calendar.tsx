2e96dee915060ef2ebff03ceba74635f39dd49d1
mcastre1 <miguelcastrejongal@gmail.com>
2026-06-08 14:06:33 -0600

    Loading dates with workouts done.

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -2,6 +2,7 @@ import WorkoutCalendar from '@/components/WorkoutCalendar';
 import { View } from "react-native";
 
 export default function Calendar() {
+
   return (
     <View style={{ flex: 1 }}>
       <WorkoutCalendar />
--- components/WorkoutCalendar.tsx
+++ components/WorkoutCalendar.tsx
@@ -1,3 +1,4 @@
+import { getDB } from "@/database/db";
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
 import { View } from "react-native";
@@ -6,22 +7,36 @@ import { CalendarList } from "react-native-calendars";
 export default function WorkoutCalendar() {
     const [selectedDate, setSelectedDate] = useState("");
 
+
     useEffect(() => {
         console.log(selectedDate);
     });
-    
+
+    // For sqlite expo api, we need to call our query from an async function
+    // but it will break expo's tab navigation so it we use a hook for it
+    useEffect(()=> {
+        loadWorkoutDates();
+    });
+
+    async function loadWorkoutDates() {
+        const db = await getDB();
+        const workouts = await db.getAllAsync("SELECT * FROM workouts")
+        console.log(workouts);
+    }
+    //////////////////////////////////////////////////////
+
     return (
-        <View style={{ flex:1 }}>
+        <View style={{ flex: 1 }}>
             <CalendarList
                 onDayPress={(day) => {
                     setSelectedDate(day.dateString);
                     router.push({
                         pathname: "/(modals)/editWorkout",
-                        params: {date : day.dateString},
+                        params: { date: day.dateString },
                     });
                 }}
                 markedDates={{
-                    [selectedDate]: {selected: true, selectedColor: "#4CAF50"}
+                    [selectedDate]: { selected: true, selectedColor: "#4CAF50" }
                 }}
                 theme={{
                     todayTextColor: "#FF5722",
