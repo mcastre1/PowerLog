b3b399b7c34297f852e176768ede195ff6bcf32b
Miguel Castrejon <miguelcastrejongal@gmail.com>
2026-06-21 20:42:27 -0600

    Created theme profile files.

---
+++ src/constants/theme/dark.ts
@@ -0,0 +1,8 @@
+export const darkTheme = {
+    mode: "dark",
+    colors: {
+        background: "#000000",
+        text: "#ffffff",
+        primary: "#60A5FA",
+        card: "#1A1A1A",
+    },};
---
+++ src/constants/theme/light.ts
@@ -0,0 +1,8 @@
+export const lightTheme = {
+    mode: "light",
+    colors: {
+        background: "#FFFFFF",
+        text: "#111111",
+        card: "#F2F2F2",
+        primary: "#3B82F6",
+    }, };
