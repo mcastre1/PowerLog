078a8bed4506ed99aaf25a57df213436b2266e7c
Miguel Castrejon <miguelcastrejongal@gmail.com>
2026-07-03 19:12:24 -0600

    Upgraded to sdk 57, fixed dependencies, and fully commited to expo router and removed react navigation

--- components/WorkoutCalendar.tsx
+++ components/WorkoutCalendar.tsx
@@ -1,8 +1,7 @@
 import { getDB } from "@/database/db";
 import { useTheme } from "@/src/constants/theme/useTheme";
-import { useIsFocused } from "@react-navigation/native";
-import { router } from "expo-router";
-import { useEffect, useState } from "react";
+import { router, useFocusEffect } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
 import { View } from "react-native";
 import { CalendarList } from "react-native-calendars";
 
@@ -30,11 +29,17 @@ type WorkoutRow = {
 };
 
 export default function WorkoutCalendar() {
-    const isFocused = useIsFocused();                      // Used to check if screen is focused.
     const [workoutDates, setWorkoutDates] = useState<string[]>([]);
     const [markedDates, setMarkedDates] = useState<Record<string, any>>()
 
     const { theme } = useTheme(); // Custom hook to get the current theme (light or dark) from the ThemeContext.
+
+    useFocusEffect(
+        useCallback(() => {
+            loadWorkoutDates();
+        }, [])
+    );
+
 
     useEffect(() => {
         const marked = workoutDates.reduce<Record<string, any>>((acc, date) => {
@@ -46,13 +51,6 @@ export default function WorkoutCalendar() {
         console.log(workoutDates);
     }, [workoutDates])
 
-    // For sqlite expo api, we need to call our query from an async function
-    // but it will break expo's tab navigation so it we use a hook for it
-    useEffect(() => {
-        if (isFocused) {
-            loadWorkoutDates();
-        }
-    }, [isFocused]);
 
     // Retrieving all dates from workouts, and saving them in workoutDates.
     // for later use
@@ -83,6 +81,8 @@ export default function WorkoutCalendar() {
                     dayTextColor: theme.colors.text,
                     monthTextColor: theme.colors.text,
                     textSectionTitleColor: theme.colors.text,
+                    todayTextColor: "#FF5722",
+                    selectedDayBackgroundColor: "4CAF50",
                 }}
                 onDayPress={(day) => {
                     router.push({
@@ -91,10 +91,6 @@ export default function WorkoutCalendar() {
                     });
                 }}
                 markedDates={markedDates ?? {}}
-                theme={{
-                    todayTextColor: "#FF5722",
-                    selectedDayBackgroundColor: "4CAF50",
-                }}
                 pastScrollRange={12}
                 futureScrollRange={12}
             />
