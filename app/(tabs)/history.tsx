b0f4771f9640a7faa4e449cde96afffbd6ce6921
Miguel Castrejon <miguelcastrejongal@gmail.com>
2026-07-09 21:08:35 -0600

    Added theme components to history tab, creating dev client.

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,5 +1,11 @@
+import { useTheme } from '@/src/constants/theme/useTheme';
 import { Text } from 'react-native';
 
 export default function History() {
-  return (<Text>History Page</Text>);
+  const { theme } = useTheme(); // Get the current theme (light or dark) from the ThemeContext.
+  return <>
+    <view style={{ flex: 1, backgroundColor: theme.colors.background, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: theme.colors.text }}>History ssPage</Text>
+    </view>
+  </>
 }
