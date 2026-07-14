e03388d7f701d2b0178c62ece0cfa4685e98c0ca
Miguel Castrejon <miguelcastrejongal@gmail.com>
2026-07-13 20:57:04 -0600

    Restructuring our local storage, going from sqlite to mmkv

--- database/queue.ts
+++
@@ -1,8 +0,0 @@
-// queue.ts
-
-let queue: Promise<any> = Promise.resolve();
-
-export function enqueue<T>(task: () => Promise<T>): Promise<T> {
-  queue = queue.then(() => task());
-  return queue;
-}
