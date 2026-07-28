4d0739d3a8f12c0082da43f5d57f021d48ec03ee
mcastre1 <miguelcastrejongal@gmail.com>
2026-07-28 09:27:25 -0600

    Added code to remove workouts from asyncstorage for testing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,20 @@
 import { useTheme } from "@/src/constants/theme/useTheme";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { ThemeProvider } from "../src/constants/theme/themeProvider";
 
+
 export default function RootLayout() {
+    async function clearWorkoutData() {
+        const keys = await AsyncStorage.getAllKeys();
+        const workoutKeys = keys.filter(key => key.startsWith("workout:"));
+        await AsyncStorage.multiRemove(workoutKeys);
+    }
+
+    clearWorkoutData()
+
     return (
         <ThemeProvider>
             <RootLayoutNav />
@@ -15,24 +25,24 @@ export default function RootLayout() {
 function RootLayoutNav() {
     const { theme } = useTheme(); // Get the current theme (light or dark) from the ThemeContext.
     return (<>
-            <SafeAreaProvider>
-                <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
-                    <StatusBar
-                        //backgroundColor="transparent" 
-                        />
-                    <Stack>
-                        <Stack.Screen
-                            name="(tabs)"
-                            options={{ headerShown: false }} />
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
+                <StatusBar
+                //backgroundColor="transparent" 
+                />
+                <Stack>
+                    <Stack.Screen
+                        name="(tabs)"
+                        options={{ headerShown: false }} />
 
-                        <Stack.Screen
-                            name="(modals)"
-                            options={{
-                                headerShown: false,
-                            }} />
-                    </Stack>
-                </SafeAreaView>
-            </SafeAreaProvider>
+                    <Stack.Screen
+                        name="(modals)"
+                        options={{
+                            headerShown: false,
+                        }} />
+                </Stack>
+            </SafeAreaView>
+        </SafeAreaProvider>
     </>
     );
 }
